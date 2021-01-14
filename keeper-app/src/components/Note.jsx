f91d447c81f85a5728d4c07c3659c21506377029
Alejandro Schmechel <alejandro.schmechel@Alejandros-MacBook-Pro.local>
2021-01-14 17:43:56 -0300

    progressing on studies, now on React

---
+++ keeper-app/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+  return (
+    <div className="note">
+      <h1>Nota 1</h1>
+      <p>bla bla bla bla bla</p>
+    </div>
+  )
+}
+
+export default Note;
